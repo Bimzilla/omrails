6b62dca4d9b16d52c5e17a016144746d466e3e28
Brian Bimschleger <bimschleger@gmail.com>
2013-05-06 16:54:07 -0400

    Added weekly pages for Design class, and added in dropdown menus for mobile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -46,4 +46,50 @@ class PagesController < ApplicationController
     #content
     #rules
   end  
+
+  def design
+    #content
+    #rules
+  end 
+
+  def week1
+    #content
+    #rules
+  end 
+
+  def week2
+    #content
+    #rules
+  end 
+
+  def week3
+    #content
+    #rules
+  end 
+
+  def week4
+    #content
+    #rules
+  end 
+
+  def week5
+    #content
+    #rules
+  end 
+
+  def week6
+    #content
+    #rules
+  end 
+
+  def week7
+    #content
+    #rules
+  end 
+  
+  def week8
+    #content
+    #rules
+  end 
+
 end
